46e8d070e3c6fc04214756dca8dfd1ce0e6fc45e
chemmeri <chemmeri@3b7b5c3b-840b-45a0-a198-780f87f84819>
2011-09-12 17:10:17 +0000

    add databases dir to repository

--- bin/initialize_repository.pl
+++ bin/initialize_repository.pl
@@ -23,12 +23,18 @@ if ( -e $file_repository ) {
   foreach my $l1 ( @hash ) {
     foreach my $l2 ( @hash ) {
     -e "$file_repository/$l1/$l2" 
-      or die "Repository subdirectory $l1/$l2 does not exit. Remove the repository or repair by hand.\n";
+      or die "Repository subdirectory $l1/$l2 does not exist. Remove the repository or repair by hand.\n";
     -r "$file_repository/$l1/$l2" and -w "$file_repository/$l1/$l2" and -x "$file_repository/$l1/$l2" 
       or die "Repository subdirectory $l1/$l2 does not have rwx permissions for this user. Remove the repository or repair by hand.\n";
     }
   }
 
+  foreach ( qw( databases downloads ) ) {
+    -e "$file_repository/$_" or die "Repository subdirectory $_ does not exist. Remove the repository or repair by hand.\n";
+    -r "$file_repository/$_" and -w "$file_repository/$_" and -x "$file_repository/$_" 
+      or die "Repository subdirectory $_ does not have rwx permissions for this user. Remove the repository or repair by hand.\n";
+  }
+  
   print "Repository exists and is configured correctly.\n";
   exit 0;
 }
@@ -39,4 +45,7 @@ foreach my $l1 ( @hash ) {
   }
 }
 
-File::Path::make_path( "$file_repository/downloads" );
+foreach ( qw( databases downloads ) ) {
+  File::Path::make_path( "$file_repository/$_" );
+}
+  
