9d852b00294cc432e44ea79649122f2c1017f446
chemmeri <chemmeri@3b7b5c3b-840b-45a0-a198-780f87f84819>
2011-10-05 12:20:32 +0000

    new sql update script

---
+++ sql/update-1.4-to-1.5.sql
@@ -0,0 +1,23 @@
+SET SESSION client_min_messages TO 'warning';
+
+-------------------------------------------------------------------
+-------------------------------------------------------------------
+-- Create archived tag column for file collections
+-------------------------------------------------------------------
+-------------------------------------------------------------------
+ALTER TABLE filecollection ADD COLUMN archive_id INTEGER;
+
+-------------------------------------------------------------------
+-------------------------------------------------------------------
+-- Create file format and type for compressed archives
+-------------------------------------------------------------------
+-------------------------------------------------------------------
+INSERT INTO fileformat ( fileformat_name, fileformat_extension, fileformat_help, fileformat_isbinary )
+  VALUES ( 'Compressed Tar Archive', 'tar.gz', 
+  	   'A compressed archive format for storing multiple files as a single file', TRUE );
+
+INSERT INTO fileformat ( fileformat_name, fileformat_extension, fileformat_help, fileformat_isbinary )
+  VALUES ( 'Tar Archive', 'tar', 'An archive format for storing multiple files as a single file', TRUE );
+
+INSERT INTO filetype ( filetype_name, filetype_help ) VALUES 
+       ( 'Archived File Collection', 'A compressed archive of a file collection' );
